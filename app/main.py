d17a1d4fdb2d9909b63d1ab6f7f36dfcf9ec43b1
= <diogo.campos2@gmail.com>
2024-03-22 01:40:43 -0300

    Iniciando

---
+++ app/main.py
@@ -0,0 +1,4 @@
+from xml_parser import XmlParser
+if '__name__' == __name__:
+    directory_path = ""
+    obj_xml = XmlParser(directory_path)
---
+++ app/xml_parser.py
@@ -0,0 +1,17 @@
+class XmlParser:
+    def __init__(self, directory_path):
+        #mudar para o seu
+        self.directory_path = directory_path #path da pasta dos arquivos
+        
+    def find_xml_files(self):
+        """Busca todos os xmls no diretorio especifico"""
+        for item in self.directory_path:
+            print("item")
+            pass
+            
+    def parse_xml_file(self, file_path):
+        """Metodo para extrair dados de um unico arquivo xml"""
+        
+    def process_all_xmls():
+        """Metodo que processa todos os arquivos xml do dir. tipo run"""
+        
